cc8b0fa68b1842a9f9308d82705efefb7f233af6
Jeppe Frandsen <jeppe@algrens.dk>
2022-02-07 09:55:37 +0100

    Changed to only use nrfx library for nRF53 series (#22)
    * Changed to use nrfx instead of SDK
    
    * Removed critical section file
    
    * Removed nrfx
    
    * Added nrfx release 2.7.0
    
    * Updated nrfx glue implementation
    
    * Minor restructuring
    
    * Removed unused file

--- targets/TARGET_NORDIC/TARGET_NRF5x/TARGET_NRF53/us_ticker.c
+++ targets/TARGET_NORDIC/TARGET_NRF5x/TARGET_NRF53/us_ticker.c
@@ -40,7 +40,6 @@
 
 #include "us_ticker_api.h"
 #include "nrf_timer.h"
-#include "app_util_platform.h"
 #include "nrfx_common.h"
 #include "mbed_critical.h"
 
@@ -85,7 +84,7 @@ void us_ticker_init(void)
 
     NVIC_SetVector(TIMER1_IRQn, (uint32_t)us_ticker_irq_handler);
 
-    NRFX_IRQ_PRIORITY_SET(TIMER1_IRQn, APP_IRQ_PRIORITY_HIGH);
+    NRFX_IRQ_PRIORITY_SET(TIMER1_IRQn, 1);
     NRFX_IRQ_ENABLE(TIMER1_IRQn);
 
     nrf_timer_task_trigger(NRF_TIMER1, NRF_TIMER_TASK_START);
