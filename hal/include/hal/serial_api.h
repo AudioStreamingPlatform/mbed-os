61896a73c78d5c31a0059e03a9c4540db0371bb8
Anders Bjørn Nedergaard <ape.anp@gmail.com>
2022-05-25 14:43:26 +0200

    Adding UART DMA support to STM32L4 on UART 1 (#39)
    * stm32l4 uart dma rx, uart idle interrupt is missing
    
    * added UART idle interrupt handling
    added UART TX Complete interrupt handling
    
    * moved to serial_device.c, some support for F4
    
    * added deinit of DMA
    
    * disabling interrupts
    
    * removing unsupported interrupts on f4
    
    * fixed callback prototype

--- hal/include/hal/serial_api.h
+++ hal/include/hal/serial_api.h
@@ -451,6 +451,13 @@ void serial_tx_abort_asynch(serial_t *obj);
  */
 void serial_rx_abort_asynch(serial_t *obj);
 
+void serial_rx_dma_init(serial_t *obj, DMA_HandleTypeDef *hdma_rx, uint8_t* buffer, size_t buffer_size, void(*DmaCompleteCallback)(uint32_t), uint32_t id);
+void serial_tx_dma_init(serial_t *obj, DMA_HandleTypeDef *hdma_tx, void(*DmaCompleteCallback)(uint32_t), uint32_t id);
+void serial_rx_dma_free(serial_t *obj);
+void serial_tx_dma_free(serial_t *obj);
+size_t serial_tx_dma(serial_t *obj, uint8_t* buffer, size_t buffer_size);
+size_t serial_get_dma_rx_position(serial_t *obj);
+void serial_idle_callback(void);
 /**@}*/
 
 #endif
